a23159c5ecd8097d84baa5be33a993dc224e89f3
L <lijiagui159@outlook.com>
2021-06-08 08:34:04 +0800

    stream mode

---
+++ src/main/java/com.lorettax/StreamExtractionEngine/core/LinkStreamFQLMode.java
@@ -0,0 +1,9 @@
+package com.lorettax.StreamExtractionEngine.core;
+
+public class LinkStreamFQLMode {
+
+    public static final String UPDATE = "update";
+    public static final String GET = "get";
+    public static final String UPGET = "upget";
+
+}
